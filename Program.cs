145d006210f1073ae53337a6ade0385af99a4a03
Marina <marinarvasc@gmail.com>
2024-12-18 17:45:48 -0300

    teste1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EvolucaoTestes.IRPF;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReceberDados();
+
+        void ReceberDados()
+        {
+        Console.Write("Olá! Qual seu nome? ");
+        string nome = Console.ReadLine()!;
+        if (nome == "")
+        {
+            System.Console.WriteLine("Digite um nome válido.");
+        }
+        Console.Write("E qual seu salário bruto? ");
+        string salBrutoString = Console.ReadLine()!;
+        double salBruto = double.Parse(salBrutoString)!;
+        if (salBrutoString == "")
+        {
+            System.Console.WriteLine("Digite um valor válido.");
+        }
+        
+        }
+
+    }
+}
