6f47d6488b0d4bf3c786cd5fbf76733e9531e952
Marina <marinarvasc@gmail.com>
2024-12-19 15:57:33 -0300

    melhorando cálculos

--- Program.cs
+++ Program.cs
@@ -4,69 +4,95 @@ using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
-    static void Main(string[] args, double salBruto)
+    static void Main(string[] args)
     {
+        double salBruto = 0;
+        string nome = "";
+
         ReceberDados();
 
         void ReceberDados()
         {
-        Console.Write("Olá! Qual seu nome? ");
-        string nome = Console.ReadLine()!;
-        if (nome == "")
-        {
-            System.Console.WriteLine("Digite um nome válido. Digite qualquer tecla para voltar.");
-            Console.ReadKey();
-            Console.Clear();
-            ReceberDados();
-        }
-        Console.Write("E qual seu salário bruto? ");
-        string salBrutoString = Console.ReadLine()!;
-         if (salBrutoString == "")
-        {
-            System.Console.WriteLine("Digite um valor válido. Digite qualquer tecla para voltar.");
-            Console.ReadKey();
-            Console.Clear();
-            ReceberDados();
-        }
-        double salBruto = double.Parse(salBrutoString)!;
+            Console.Write("Olá! Qual seu nome? ");
+            string nome = Console.ReadLine()!;
+            if (nome == "")
+            {
+                System.Console.WriteLine("\nDigite um nome válido. \nDigite qualquer tecla para voltar.");
+                Console.ReadKey();
+                Console.Clear();
+                ReceberDados();
+            }
+            Console.Write("E qual seu salário bruto? ");
+            string salBrutoString = Console.ReadLine()!;
+
+            try
+            {
+                salBruto = double.Parse(salBrutoString);
+                if (salBruto <= 0)
+                {
+                    throw new Exception("Digite um valor válido.");
+                }
+                CalcularImpostoRenda(salBruto, nome);
+            }
+            catch
+            {
+                Console.WriteLine("\nValor inválido. ");
+                Console.WriteLine("Digite qualquer tecla para voltar.");
+                Console.ReadKey();
+                Console.Clear();
+                ReceberDados();
+            }
         }
 
-        CalcularImpostoRenda(salBruto);
-        
-            void CalcularImpostoRenda(double salBruto)
+        CalcularImpostoRenda(salBruto, nome);
+
+        void CalcularImpostoRenda(double salBruto, string nome)
         {
-            double imposto = 0;
+            double desconto = 0;
             double salLiquido = 0;
+            double aliquota = 0;
+            double deducao = 0;
 
-            if (salBruto <= 1903.98) 
-        {
-            imposto = 0;
-            salLiquido = salBruto;
-        }
+            if (salBruto <= 1903.98)
+            {
+                desconto = 0;
+                salLiquido = salBruto;
+            }
             else if (salBruto <= 2826.65)
-        {
-            imposto = salBruto * 0.075; 
-            salLiquido = salBruto - imposto;
-        }
+            {
+                aliquota = 0.075;
+                deducao = 142.80;
+                desconto = salBruto * aliquota - deducao;
+                salLiquido = salBruto - desconto;
+            }
             else if (salBruto <= 3751.05)
-        {
-            imposto = salBruto * 0.15; 
-            salLiquido = salBruto - imposto;
-        }
-             else if (salBruto <= 4664.68)
-        {
-            imposto = salBruto * 0.225;
-            salLiquido = salBruto - imposto; 
-        }
-             else
-        {
-            imposto = salBruto * 0.275; 
-            salLiquido = salBruto - imposto;
-        }
+            {
+                aliquota = 0.15;
+                deducao = 354.80;
+                desconto = salBruto * aliquota - deducao;
+                salLiquido = salBruto - desconto;
+            }
+            else if (salBruto <= 4664.68)
+            {
+                aliquota = 0.225;
+                deducao = 636.13;
+                desconto = salBruto * aliquota - deducao;
+                salLiquido = salBruto - desconto;
+            }
+            else
+            {
+                aliquota = 0.275;
+                deducao = 869.36;
+                desconto = salBruto * aliquota - deducao;
+                salLiquido = salBruto - desconto;
+            }
 
-        Console.WriteLine("\nSalário bruto: " + salBruto);
-        System.Console.WriteLine("\nImposto: " + imposto);
-        Console.WriteLine("\nSalário líquido: " + salLiquido);
+            Console.WriteLine("\nNome do contribuinte: " + nome);
+            Console.WriteLine("Salário bruto: " + salBruto);
+            System.Console.WriteLine("Desconto: " + desconto);
+            Console.WriteLine("Salário líquido: " + salLiquido);
+            
+            
         }
     }
 }
