02a7b2d82913cdf5700950862414cc4fd4fa8672
Marina <marinarvasc@gmail.com>
2024-12-26 17:47:18 -0300

    deletando obj e bin

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 using System.Security.Cryptography.X509Certificates;
 namespace EvolucaoTestes.IRPF;
 
@@ -16,32 +17,41 @@ class Program
         }
         else
         {
-            if (double.TryParse(num, out double resultado))
-        {
-            for (int i = 0; i < resultado; i++)
+            try
             {
-                AnalisarNome analisar = new AnalisarNome("", 0);
-                analisar.ReceberNome();
+            if (double.TryParse(num, out double resultado))
+            {
+                for (int i = 0; i < resultado; i++)
+                {
+                    AnalisarNome analisar = new AnalisarNome("", 0);
+                    analisar.ReceberNome();
 
-                double SalBruto = analisar.SalBruto;
-                string Nome = analisar.Nome;
+                    double SalBruto = analisar.SalBruto;
+                    string Nome = analisar.Nome;
 
-                CalculoImpostoRenda calculo = new CalculoImpostoRenda();
-                calculo.CalcularImpostoRenda(SalBruto, Nome);
+                    CalculoImpostoRenda calculo = new CalculoImpostoRenda();
+                    calculo.CalcularImpostoRenda(SalBruto, Nome);
 
-                System.Console.WriteLine($"Nome: {analisar.Nome}");
-                System.Console.WriteLine($"Salário bruto: {analisar.SalBruto.ToString("C2")}");
-                System.Console.WriteLine($"Salário líquido: {calculo.salLiquido.ToString("C2")}");
-                System.Console.WriteLine($"Imposto: {calculo.desconto.ToString("C2")}");
+                    System.Console.WriteLine($"Nome: {analisar.Nome}");
+                    System.Console.WriteLine($"Salário bruto: {analisar.SalBruto.ToString("C2")}");
+                    System.Console.WriteLine($"Salário líquido: {calculo.salLiquido.ToString("C2")}");
+                    System.Console.WriteLine($"Imposto: {calculo.desconto.ToString("C2")}");
 
-                System.Console.WriteLine("\nSe deseja voltar ao íncio para calcular mais um contribuinte, digite 'sim'. \nCaso contrário, só digitar qualquer outra coisa. ");
-                string sim = Console.ReadLine()!;
-                if (sim == "sim") 
-                { Main(args); }
+                    System.Console.WriteLine("\nSe deseja voltar ao íncio para calcular mais um contribuinte, digite 'sim'. \nCaso contrário, só digitar qualquer outra coisa. ");
+                    string sim = Console.ReadLine()!;
+                    if (sim == "sim")
+                    { Main(args); }
+                }
 
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nDigite um valor válido.\nPressione qualquer tecla para voltar.");
+                Console.ReadKey();
+                Main(args);
             }
         }
-    }
 
-}
+    }
 }
