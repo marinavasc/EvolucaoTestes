20d8bd6d4fc3e1a44e2fe08b4dd4e777e7526a7c
Marina <marinarvasc@gmail.com>
2024-12-26 15:33:49 -0300

    extra

--- obj/Debug/net8.0/EvolucaoTestes.IRPF.AssemblyInfo.cs
+++ obj/Debug/net8.0/EvolucaoTestes.IRPF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvolucaoTestes.IRPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4946fdd71aef643858034de8f900b17ed8adbae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c6a143a7fe3517de97eb834dbb4168a8be2109")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvolucaoTestes.IRPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvolucaoTestes.IRPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
