7fc4450b79354a60a74b065343529cbb59002c2b
Marina <marinarvasc@gmail.com>
2024-12-27 14:06:19 -0300

    consertando program

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ class Program
     {
         Console.Write("\nOlá! Quantos contribuintes iremos calcular? ");
         string num = Console.ReadLine()!;
+
         if (num == "0")
         {
             Console.WriteLine("\nDigite um valor válido.\nPressione qualquer tecla para voltar.");
@@ -19,29 +20,28 @@ class Program
         {
             try
             {
-            if (double.TryParse(num, out double resultado))
-            {
-                for (int i = 0; i < resultado; i++)
+                if (double.TryParse(num, out double resultado))
                 {
-                    AnalisarNome analisar = new AnalisarNome("", 0);
-                    analisar.ReceberNome();
+                    for (int i = 0; i < resultado; i++)
+                    {
+                        AnalisarNome analisar = new AnalisarNome("", 0);
+                        analisar.ReceberNome();
 
-                    double SalBruto = analisar.SalBruto;
-                    string Nome = analisar.Nome;
+                        double SalBruto = analisar.SalBruto;
+                        string Nome = analisar.Nome;
 
-                    CalculoImpostoRenda calculo = new CalculoImpostoRenda();
-                    calculo.CalcularImpostoRenda(SalBruto, Nome);
+                        CalculoImpostoRenda calculo = new CalculoImpostoRenda();
+                        calculo.CalcularImpostoRenda(SalBruto, Nome);
 
-                    System.Console.WriteLine($"Nome: {analisar.Nome}");
-                    System.Console.WriteLine($"Salário bruto: {analisar.SalBruto.ToString("C2")}");
-                    System.Console.WriteLine($"Salário líquido: {calculo.salLiquido.ToString("C2")}");
-                    System.Console.WriteLine($"Imposto: {calculo.desconto.ToString("C2")}");
-
-                    System.Console.WriteLine("\nSe deseja voltar ao íncio para calcular mais um contribuinte, digite 'sim'. \nCaso contrário, só digitar qualquer outra coisa. ");
+                        System.Console.WriteLine($"Nome: {analisar.Nome}");
+                        System.Console.WriteLine($"Salário bruto: {analisar.SalBruto.ToString("C2")}");
+                        System.Console.WriteLine($"Salário líquido: {calculo.salLiquido.ToString("C2")}");
+                        System.Console.WriteLine($"Imposto: {calculo.desconto.ToString("C2")}");
+                    }
+                    
+                    Console.WriteLine("\nSe deseja voltar ao íncio para calcular mais um contribuinte, digite 'sim'. \nCaso contrário, só digitar qualquer outra coisa. ");
                     string sim = Console.ReadLine()!;
-                    if (sim == "sim")
-                    { Main(args); }
-                }
+                    if (sim == "sim") Main(args); 
 
                 }
             }
@@ -55,3 +55,4 @@ class Program
 
     }
 }
+
